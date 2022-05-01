e5c8f76bf0945ada4999a11d0dea87e930b2a1ee
dlepe <david.lepe94@gmail.com>
2022-05-01 08:49:04 -0700

    Initial commit

---
+++ src/main/java/com/dlepe/TwitchChatAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.dlepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchChatAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchChatAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlepe/TwitchChatAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlepe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchChatAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
